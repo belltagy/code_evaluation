084d2bc7efce026802e98a4c8875b82895f61c43
mohamed beltagy <moh.bell@crowdbotics.com>
2021-10-14 02:28:56 -1000

    add string reprsentation for models

--- books/models.py
+++ books/models.py
@@ -5,8 +5,14 @@ class Author(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
 
+    def __str__(self):
+        return self.name
+
 class Publisher(models.Model):
     name = models.CharField(max_length=300)
+    
+    def __str__(self):
+        return self.name
 
 class Book(models.Model):
     name = models.CharField(max_length=300)
@@ -17,6 +23,12 @@ class Book(models.Model):
     publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
     pubdate = models.DateField()
 
+    def __str__(self):
+        return self.name
+
 class Store(models.Model):
     name = models.CharField(max_length=300)
     books = models.ManyToManyField(Book)
+
+    def __str__(self):
+        return self.name
